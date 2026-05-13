61c5b1fba28620a0551a9bf4555ca0826734de27
lenovo <amohamed@soforx.com>
2026-05-13 19:45:10 +0300

    Update angular configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,40 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { Observable } from 'rxjs';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+export class AppTranslateLoader implements TranslateLoader {
+  constructor(private http: HttpClient) {}
+
+  getTranslation(lang: string): Observable<any> {
+    return this.http.get(`/assets/i18n/${lang}.json`);
+  }
+   }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'ar',
+        loader: {
+          provide: TranslateLoader,
+          useClass: AppTranslateLoader,
+          deps: [HttpClient],
+        },
+      })
+    ),
+  ],
 };
---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,70 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+
+
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [TranslateModule,MatButtonModule,
+     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar implements OnInit {
+
+  private translate = inject(TranslateService);
+
+  currentLang = 'ar';
+
+  ngOnInit(): void {
+
+    this.translate.setDefaultLang('ar');
+
+    this.translate.use(this.currentLang);
+
+    document.documentElement.lang =
+      this.currentLang;
+
+    document.documentElement.dir =
+      'rtl';
+  }
+
+  changeLang() {
+
+    this.currentLang =
+      this.currentLang === 'ar'
+        ? 'en'
+        : 'ar';
+
+    this.translate.use(this.currentLang);
+
+    document.documentElement.lang =
+      this.currentLang;
+
+    document.documentElement.dir =
+      this.currentLang === 'ar'
+        ? 'rtl'
+        : 'ltr';
+  }
+
+  private fb =inject(FormBuilder)
+
+  form = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  submit() {
+    alert(this.form.value)
+  }
+}
