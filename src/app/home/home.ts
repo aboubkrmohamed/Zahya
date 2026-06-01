825f86c2d42a24f64f0d80998fb0a9d54a729a18
lenovo <amohamed@soforx.com>
2026-06-01 20:03:23 +0300

    Update angular configuration

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,139 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface Product {
+  id: number;
+  name: string;
+  category: string;
+  image: string;
+  oldPrice: number;
+  price: number;
+  rating: number;
+  reviewsCount: number;
+
+  isNew?: boolean;
+  isHot?: boolean;
+  discount?: number;
+
+  outOfStock?: boolean;
+
+  sizes?: string[];
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home{
+  products: Product[] = [
+    {
+      id: 1,
+      category: 'RADIANCE',
+      name: 'Brightening Serum Cream',
+      image: 'assets/imgs/product-img-1.png',
+      oldPrice: 200,
+      price: 176,
+      rating: 5,
+      reviewsCount: 50,
+      isNew: true,
+      outOfStock: true,
+    },
 
+    {
+      id: 2,
+      category: 'RADIANT',
+      name: 'Hydrating Red Lip Stain',
+      image: 'assets/imgs/product-img-2.png',
+      oldPrice: 200,
+      price: 187,
+      rating: 5,
+      reviewsCount: 50,
+      isNew: true,
+    },
+
+    {
+      id: 3,
+      category: 'CRYSTAL',
+      name: 'Prism Eyeshadow Palette',
+      image: 'assets/imgs/product-img-3.png',
+      oldPrice: 200,
+      price: 170,
+      rating: 5,
+      reviewsCount: 50,
+      discount: 15,
+    },
+
+    {
+      id: 4,
+      category: 'CRYSTAL GLEAM',
+      name: 'Crystal-Infused Highlighter',
+      image: 'assets/imgs/product-img-4.png',
+      oldPrice: 200,
+      price: 150,
+      rating: 5,
+      reviewsCount: 50,
+      isHot: true,
+      discount: 15,
+      sizes: ['ml 1500', 'ml 1000', 'ml 500', 'ml 250'],
+    },
+
+     {
+      id: 4,
+      category: 'CRYSTAL GLEAM',
+      name: 'Crystal-Infused Highlighter',
+      image: 'assets/imgs/product-img-5.png',
+      oldPrice: 200,
+      price: 150,
+      rating: 5,
+      reviewsCount: 50,
+      isHot: true,
+      discount: 15,
+      sizes: ['ml 1500', 'ml 1000', 'ml 500', 'ml 250'],
+    },
+
+         {
+      id: 4,
+      category: 'CRYSTAL GLEAM',
+      name: 'Crystal-Infused Highlighter',
+      image: 'assets/imgs/product-img-6.png',
+      oldPrice: 200,
+      price: 150,
+      rating: 5,
+      reviewsCount: 50,
+      isHot: true,
+      discount: 15,
+      sizes: ['ml 1500', 'ml 1000', 'ml 500', 'ml 250'],
+    },
+
+             {
+      id: 4,
+      category: 'CRYSTAL GLEAM',
+      name: 'Crystal-Infused Highlighter',
+      image: 'assets/imgs/product-img-7.png',
+      oldPrice: 200,
+      price: 150,
+      rating: 5,
+      reviewsCount: 50,
+      isHot: true,
+      discount: 15,
+      sizes: ['ml 1500', 'ml 1000', 'ml 500', 'ml 250'],
+    },
+
+             {
+      id: 4,
+      category: 'CRYSTAL GLEAM',
+      name: 'Crystal-Infused Highlighter',
+      image: 'assets/imgs/product-img-8.png',
+      oldPrice: 200,
+      price: 150,
+      rating: 5,
+      reviewsCount: 50,
+      isHot: true,
+      discount: 15,
+      sizes: ['ml 1500', 'ml 1000', 'ml 500', 'ml 250'],
+    },
+  ];
 }
